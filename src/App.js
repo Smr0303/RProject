1bfb8cd63f5c66a8abf723bbe27f471062217a24
Smr0303 <samarmohite7@gmail.com>
2023-11-21 20:30:44 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,343 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useDisclosure } from "@chakra-ui/react";
+import { useToast } from "@chakra-ui/react";
+import { useState } from "react";
+import {
+  Stack,
+  Input,
+  Select,
+  FormControl,
+  FormLabel,
+  Button,
+  Radio,
+  RadioGroup,
+  Heading,
+} from "@chakra-ui/react";
+import { Fade, ScaleFade, Slide, SlideFade, Collapse } from "@chakra-ui/react";
+
+import axios from "axios";
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
 
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [age, setAge] = useState("");
+  const [cholestrol, setCholestrol] = useState("");
+  const [bPSystolic, setbPSystolic] = useState("");
+  const [bP_Di_Astolic, setbP_Di_Astolic] = useState("");
+  const [heartRate, setHeartRate] = useState("");
+  const [diabetes, setDiabetes] = useState("");
+  const [familyHistory, setFamilyHistory] = useState("");
+  const [smoking, setSmoking] = useState("");
+  const [obesity, setObesity] = useState("");
+  const [alcohol, setAlcohol] = useState("");
+  const [heartProblem, setHeartProblem] = useState("");
+  const [medication_Use, setMedication_Use] = useState("");
+  const [bmi, setBmi] = useState("");
+  const [triglycerides, setTriglycerides] = useState("");
+  const [sleep_hours, setSleep_hours] = useState("");
+  const [diet, setDiet] = useState("");
+  const [gender, setGender] = useState("");
+  const [output, setOutput] = useState("Click on Predict");
+
+  const toast = useToast();
+
+  async function handlePredict() {
+    const object = {
+      Age: age,
+      Cholestrol: cholestrol,
+      BP_systolic: bPSystolic,
+      BP_diastolic: bP_Di_Astolic,
+      "Heart Rate": heartRate,
+      Diabetes: parseInt(diabetes),
+      "Family History": parseInt(familyHistory),
+      Smoking: parseInt(smoking),
+      Obesity: parseInt(obesity),
+      "Alcohol Consumption": parseInt(alcohol),
+      "Exercise Hours Per Week": 20,
+      "Previous Heart Problems": parseInt(heartProblem),
+      "Medication Use": parseInt(medication_Use),
+      BMI: bmi,
+      Triglycerides: triglycerides,
+      "Sleep Hours Per Day": sleep_hours,
+      Sex: gender,
+      Diet: parseInt(diet),
+    };
+    console.log(object);
+    try {
+      toast({
+        title: "Model Predicting",
+        description: "Predicting Output........   ",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
+      const response = await axios.post(
+        "http://localhost:8000/predict",
+        object
+      );
+      if (response.data == "1") {
+        setOutput("You are at risk of Heart Attack");
+      } else {
+        setOutput("You are fit");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    console.log("Predicting");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: "100vw" }}>
+      <Heading as="h2" colorScheme="blue" size="3xl" noOfLines={1}>
+        HeartAttack Prediction Model
+      </Heading>
+      <div
+        className="InputGroup"
+        style={{
+          display: "flex",
+          width: "100vw",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <div className="Box1">
+          <FormControl m={2} variant="floating" id="Age">
+            <FormLabel htmlFor="Age">Age</FormLabel>
+            <Input
+              m={2}
+              id="Age"
+              variant="outline"
+              size="md"
+              placeholder="Enter Your Age"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+              _placeholder={{ opacity: 1, color: "gray.500" }}
+            />
+          </FormControl>
+          <FormControl m={2} variant="floating" id="Cholestrol">
+            <FormLabel htmlFor="Cholestrol">Cholestrol</FormLabel>
+            <Input
+              m={2}
+              id="Cholestrol"
+              variant="outline"
+              size="md"
+              placeholder="Enter Cholestrol Level"
+              value={cholestrol}
+              onChange={(e) => setCholestrol(e.target.value)}
+              _placeholder={{ opacity: 1, color: "gray.500" }}
+            />
+          </FormControl>
+          <FormControl m={2} variant="floating" id="bPSystolic">
+            <FormLabel htmlFor="floating-input">BP Systolic</FormLabel>
+            <Input
+              m={2}
+              id="bPSystolic"
+              variant="outline"
+              size="md"
+              value={bPSystolic}
+              onChange={(e) => setbPSystolic(e.target.value)}
+              placeholder="bPSystolic"
+              _placeholder={{ opacity: 1, color: "gray.500" }}
+            />
+          </FormControl>
+          <FormControl m={2} variant="floating" id="bP_Di_Astolic">
+            <FormLabel htmlFor="bP_Di_Systolic">BP-Diastolic</FormLabel>
+            <Input
+              m={2}
+              id="bP_Di_Systolic"
+              variant="outline"
+              size="md"
+              value={bP_Di_Astolic}
+              onChange={(e) => setbP_Di_Astolic(e.target.value)}
+              placeholder="Enter BP-Diastolic Level"
+              _placeholder={{ opacity: 1, color: "gray.500" }}
+            />
+          </FormControl>
+          <FormControl m={2} variant="floating" id="Heart Rate">
+            <FormLabel htmlFor="floating-input">Heart Rate</FormLabel>
+            <Input
+              m={2}
+              id="floating-input"
+              variant="outline"
+              size="md"
+              value={heartRate}
+              onChange={(e) => setHeartRate(e.target.value)}
+              placeholder=" Enter Heart Rate"
+              _placeholder={{ opacity: 1, color: "gray.500" }}
+            />
+          </FormControl>
+
+          <FormControl m={2}>
+            <FormLabel> Have Diabetes?</FormLabel>
+            <RadioGroup m={2} value={diabetes} onChange={setDiabetes}>
+              <Stack spacing={4} direction="row">
+                <Radio value="1">Yes</Radio>
+                <Radio value="0">No</Radio>
+              </Stack>
+            </RadioGroup>
+          </FormControl>
+
+          <FormControl m={2} variant="floating" id="Family History">
+            <FormLabel htmlFor="floating-input">Family History</FormLabel>
+            <RadioGroup m={2} value={familyHistory} onChange={setFamilyHistory}>
+              <Stack spacing={4} direction="row">
+                <Radio value="1">Yes</Radio>
+                <Radio value="0">No</Radio>
+              </Stack>
+            </RadioGroup>
+          </FormControl>
+
+          <FormControl m={2} variant="floating" id="Smoking">
+            <FormLabel htmlFor="floating-input">Smoking</FormLabel>
+            <RadioGroup m={2} value={smoking} onChange={setSmoking}>
+              <Stack spacing={4} direction="row">
+                <Radio value="1">Yes</Radio>
+                <Radio value="0">No</Radio>
+              </Stack>
+            </RadioGroup>
+          </FormControl>
+        </div>
+
+        <div className="Box2">
+          <FormControl m={2} variant="floating" id="Obesity">
+            <FormLabel htmlFor="floating-input">Obesity</FormLabel>
+            <RadioGroup m={2} value={obesity} onChange={setObesity}>
+              <Stack spacing={4} direction="row">
+                <Radio value="1">Yes</Radio>
+                <Radio value="0">No</Radio>
+              </Stack>
+            </RadioGroup>
+          </FormControl>
+
+          <FormControl m={2} variant="floating" id="Alcohol">
+            <FormLabel htmlFor="floating-input">Alcohol</FormLabel>
+            <RadioGroup m={2} value={alcohol} onChange={setAlcohol}>
+              <Stack spacing={4} direction="row">
+                <Radio value="1">Yes</Radio>
+                <Radio value="0">No</Radio>
+              </Stack>
+            </RadioGroup>
+          </FormControl>
+
+          <FormControl m={2} variant="floating" id="heartProblem">
+            <FormLabel htmlFor="floating-input">
+              Previous heart Problem
+            </FormLabel>
+            <RadioGroup m={2} value={heartProblem} onChange={setHeartProblem}>
+              <Stack spacing={4} direction="row">
+                <Radio value="1">Yes</Radio>
+                <Radio value="0">No</Radio>
+              </Stack>
+            </RadioGroup>
+          </FormControl>
+          <FormControl m={2} variant="floating" id="Diet">
+            <FormLabel htmlFor="floating-input">Diet </FormLabel>
+            <RadioGroup m={2} value={diet} onChange={setDiet}>
+              <Stack spacing={4} direction="row">
+                <Radio value="1">Yes</Radio>
+                <Radio value="0">No</Radio>
+              </Stack>
+            </RadioGroup>
+          </FormControl>
+
+          <FormControl m={2} variant="floating" id="Gender">
+            <FormLabel htmlFor="floating-input">Enter Gender </FormLabel>
+            <RadioGroup m={2} value={gender} onChange={setGender}>
+              <Stack spacing={4} direction="row">
+                <Radio value="Male">Male</Radio>
+                <Radio value="Female">Female</Radio>
+              </Stack>
+            </RadioGroup>
+          </FormControl>
+
+          <FormControl m={2} variant="floating" id="Medication_Use">
+            <FormLabel htmlFor="floating-input">Use Medicines </FormLabel>
+            <RadioGroup
+              m={2}
+              value={medication_Use}
+              onChange={setMedication_Use}
+            >
+              <Stack spacing={4} direction="row">
+                <Radio value="1">Yes</Radio>
+                <Radio value="0">No</Radio>
+              </Stack>
+            </RadioGroup>
+          </FormControl>
+
+          <FormControl m={2} variant="floating" id="bmi">
+            <FormLabel htmlFor="bmi"> Enter Body Mass Index</FormLabel>
+            <Input
+              m={2}
+              id="bmi"
+              variant="outline"
+              size="md"
+              value={bmi}
+              onChange={(e) => setBmi(e.target.value)}
+              placeholder="Enter Body Mass Index"
+              _placeholder={{ opacity: 1, color: "gray.500" }}
+            />
+          </FormControl>
+
+          <FormControl m={2} variant="floating" id="Triglycerides">
+            <FormLabel htmlFor="floating-input">Triglycerides Level</FormLabel>
+            <Input
+              m={2}
+              id="floating-input"
+              variant="outline"
+              size="md"
+              value={triglycerides}
+              onChange={(e) => setTriglycerides(e.target.value)}
+              placeholder="Enter Triglycerides Level"
+              _placeholder={{ opacity: 1, color: "gray.500" }}
+            />
+          </FormControl>
+
+          <FormControl m={2} variant="floating" id="sleep_hours">
+            <FormLabel htmlFor="floating-input">Sleep Hours</FormLabel>
+            <Input
+              m={2}
+              id="heartProblem"
+              variant="outline"
+              size="md"
+              value={sleep_hours}
+              onChange={(e) => setSleep_hours(e.target.value)}
+              placeholder="Enter Sleep Hours Per Day"
+              _placeholder={{ opacity: 1, color: "gray.500" }}
+            />
+          </FormControl>
+        </div>
+
+        <Button onClick={onOpen} colorScheme="teal">
+          Predict
+        </Button>
+
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Predicted Output</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody> {output}</ModalBody>
+
+            <ModalFooter>
+              <Button variant="ghost" mr={3} onClick={onClose}>
+                Close
+              </Button>
+              <Button onClick={handlePredict} colorScheme="blue">
+                Predict
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//curl -X POST -H "Content-Type: application/json" -d '{"Age": 0.64160649, "Cholesterol": -0.6477889, "BP_systolic": 0.88256006, "BP_diastolic": 0.21029289, "Heart Rate": -0.14856937, "Diabetes": -1.23597753, "Family History": -0.88143929, "Smoking": 0.37132962, "Obesity": -0.89376539, "Alcohol Consumption": -1.08756891, "Exercise Hours Per Week": -1.04994223, "Previous Heart Problems": -0.87937102, "Medication Use": -0.89263941, "BMI": 0.38334592, "Triglycerides": -0.59001446, "Sleep Hours Per Day": -0.47481413, "Sex": "Male", "Diet": "Average"}' http://localhost:8000/predict
